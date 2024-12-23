26aecc06a6739afba358161bce82b17d5dbbe78f
Romain Wintgens <romain@mac-de-romain.home>
2024-12-23 13:53:23 +0100

    backup

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,14 @@
 document.getElementById('hamburger').addEventListener('click', function(event) {
     event.stopPropagation();
     var navBar = document.getElementById('navBar');
+    var hamburger = document.getElementById('hamburger');
     if (navBar.classList.contains('show')) {
         navBar.classList.remove('show');
+        hamburger.classList.remove('rotate');
         document.body.style.overflow = '';
     } else {
         navBar.classList.add('show');
+        hamburger.classList.add('rotate');
         document.body.style.overflow = 'hidden';
     }
 });
@@ -16,8 +19,10 @@ document.getElementById('navBar').addEventListener('touchmove', function(event) 
 
 document.addEventListener('click', function(event) {
     var navBar = document.getElementById('navBar');
+    var hamburger = document.getElementById('hamburger');
     if (navBar.classList.contains('show') && !navBar.contains(event.target)) {
         navBar.classList.remove('show');
+        hamburger.classList.remove('rotate');
         document.body.style.overflow = '';
     }
 });
