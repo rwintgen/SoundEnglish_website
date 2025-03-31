04d109e2259c9d53865b30f2579064ac0ffc57cc
Romain Wintgens <rwintgen@student.42madrid.com>
2025-03-31 18:24:56 +0200

    2 landings almost ready

--- js/landing.js
+++ js/landing.js
@@ -1,7 +1,21 @@
 document.getElementById("landingMainCta1").addEventListener("click", function () {
-    document.getElementById("contactSection1").scrollIntoView({ behavior: "smooth" });
+	const target = document.getElementById("contactSection1");
+	const offset = 100;
+	const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+	window.scrollTo({
+		top: targetPosition,
+		behavior: "smooth"
+	});
 });
 
 document.getElementById("landingMainCta2").addEventListener("click", function () {
-    document.getElementById("contactSection2").scrollIntoView({ behavior: "smooth" });
+	const target = document.getElementById("contactSection2");
+	const offset = 100;
+	const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+	window.scrollTo({
+		top: targetPosition,
+		behavior: "smooth"
+	});
 });
